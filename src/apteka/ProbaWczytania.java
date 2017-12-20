7539ba9e9e9ae5d3adbf3dca247fc5b857951761
Exatros <Exatros@192.168.2.56>
2017-12-20 17:00:15 +0100

    Wczytywanie pliku oraz rozdzielanie

--- src/apteka/ProbaWczytania.java
+++ src/apteka/ProbaWczytania.java
@@ -1,27 +1,30 @@
 package apteka;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class ProbaWczytania {
 
-    public static void wczytanie(String nazwa) throws FileNotFoundException {
+    public void wczytanie(String sciezka_Pliku) throws IOException {
+        String first_read[] = null;
+        String second_read[] = null;
+        try (FileReader filereader = new FileReader(sciezka_Pliku)) {
+            BufferedReader buffReader = new BufferedReader(filereader);
 
-        ArrayList<Lekarstwo> lekarstwa = new ArrayList<>();
-
-        Scanner in = new Scanner(new FileReader(nazwa));
-        String line;
-        while (in.hasNext()) {
-            line = in.nextLine();
-            String lineElements[] = line.split(";");
-
-
+            String textLine = buffReader.readLine();
+            do {
+                
+                first_read = textLine.split(";");//Pierwsze rozdzielenie separatorem ";"
+                second_read = first_read[3].split(",");//Drugie rozdzielenie seperatorem ","
+                for (String item : second_read)//Pętla po elementach tablicy 
+                {   
+                    System.out.print(item+" ");
+                }
+                System.out.println();
+                textLine = buffReader.readLine();//Czytanie kolejnej lini
+            } while (textLine != null);
         }
-        in.close();
 
     }
 
