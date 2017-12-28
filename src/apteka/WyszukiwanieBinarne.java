7155bd3d8ead09c6ee01e8256fad23fe10c9d821
Basian <Basian@Basian-Komputer>
2017-12-28 13:56:12 +0100

    Modyfikacja klasy Wyszukiwanie Binarne

--- src/apteka/WyszukiwanieBinarne.java
+++ src/apteka/WyszukiwanieBinarne.java
@@ -8,6 +8,7 @@ package apteka;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
@@ -15,20 +16,29 @@ import java.io.IOException;
  */
 public class WyszukiwanieBinarne {
     
+    private int line = 0;
+    private int [] Lista;
     
-    
-    public void wyszukanieID(String sciezka_Pliku) throws IOException {
-       
-          
-        try (FileReader filereader = new FileReader(sciezka_Pliku)) {
-            int Wersy = 0;
-            BufferedReader buffReader = new BufferedReader(filereader);
-            String line = buffReader.readLine();
             
-            while (line!=null){
-                Wersy++;
+    public int zliczenieWersow(String sciezka_Pliku) throws IOException {
+       
+        try (Scanner in = new Scanner(new FileReader(sciezka_Pliku))) {
+            while (in.hasNext()) {
+                line++;              //zliczanie wszsytkich lekarstw
             }
         }
-        
+         
+          return line;    
         }
+    
+    public void wyszukanieID() throws IOException{
+        WyszukiwanieBinarne wb = new WyszukiwanieBinarne();
+        int [] lista = new int [wb.line];
+        
+        
+        
+        
+    }
+    
+    
 }
