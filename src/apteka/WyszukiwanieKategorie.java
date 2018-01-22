c262861c60f36ff0e5a29cefd359efbe2000a930
lukaszcz <lukaszcz@mail.com>
2018-01-22 11:41:44 +0100

    Poprawki do WyszukiwanieKategorie

--- src/apteka/WyszukiwanieKategorie.java
+++ src/apteka/WyszukiwanieKategorie.java
@@ -19,43 +19,36 @@ public boolean equalsIgnoreCase(String anotherString){
     return false;
 };
 
-public static void WyszukiwanieKategorie (String sciezka, String Kategoria) throws IOException{
+public ArrayList WyszukiwanieKategorie (String sciezka, String Kategoria) throws IOException{
         
         int l;
+        String kat = Kategoria;
         ArrayList<String> listaKategorie = new ArrayList<>();
         ArrayList<String> lista = new ArrayList<>();
-        
+        ArrayList<String> wynik= new ArrayList<>();
+        ArrayList<Integer> listawynik= new ArrayList<>();
         String first_read[] = null;
         String second_read[] = null;
+        
         try (FileReader filereader = new FileReader(sciezka)) {
             BufferedReader buffReader = new BufferedReader(filereader);
 
             String textLine = buffReader.readLine();
             do {
                 
-                first_read = textLine.split(";");//Pierwsze rozdzielenie separatorem ";"
+                first_read = textLine.split(";");
                 lista.add(textLine);
-                second_read = first_read[4].split(";");//Drugie rozdzielenie seperatorem ","
-                /*for (String item : first_read)//Pętla po elementach tablicy 
-                {   
-                   //System.out.print(item+" ");
-                   lista.add(item);
-                    
-                }*/
+                second_read = first_read[4].split(";");
                 for (String item2 : second_read){
-                    //System.out.print(item2+" ");
                     listaKategorie.add(item2);
                 }
-                //System.out.println();
-                textLine = buffReader.readLine();//Czytanie kolejnej lini
+                textLine = buffReader.readLine();
             } while (textLine != null);
         }
-
-       ArrayList<Integer> listawynik= new ArrayList<>();
-       String wynik = Kategoria;
+       
        for (int i=0;i<listaKategorie.size();i++){
             String temp=(String)listaKategorie.get(i);
-            if(temp.equalsIgnoreCase(wynik)){
+            if(temp.equalsIgnoreCase(kat)){
             l=i+1;  
             listawynik.add(i);    
                 
@@ -65,16 +58,16 @@ public static void WyszukiwanieKategorie (String sciezka, String Kategoria) thro
                 listawynik.add(-1);
             }
         }
-       //System.out.println(lista.get(15));
+       
     for (int j=0;j<lista.size();j++){
         
         if(listawynik.get(j)==-1){
             continue;
         }
         else{
-            System.out.println((String)lista.get(j));
+            wynik.add(String.valueOf(lista.get(j)));
         }
     }
-        
-    }    
+      return wynik;  
+    }
 }
